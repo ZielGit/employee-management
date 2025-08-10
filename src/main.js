5ff9c104113e172461128a2ea3d4de6ba011d24c
Frans <frans_421_12@hotmail.com>
2025-08-10 12:31:16 -0500

    chore: config vuetify an icons mdi

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
 app.use(router)
+
+app.use(vuetify)
 
 app.mount('#app')
