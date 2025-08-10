76e68b3cfcf43b8de9e8c37eb203a5da43c01a13
Frans <frans_421_12@hotmail.com>
2025-08-09 20:49:30 -0500

    chore: remove config bootstrap

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-import './assets/styles.scss'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import * as bootstrap from 'bootstrap'
 
-window.bootstrap = bootstrap
 const app = createApp(App)
 
 app.use(router)
